4563ad086f89d44efb75721fab4cc4c9ec554956
devtvas <tarcisio.word@gmail.com>
2024-07-30 04:13:17 -0300

    index - implementando meu first hash

---
+++ index.js
@@ -0,0 +1,13 @@
+const sha256 = require('crypto-js/sha256');
+
+const transaction = {
+    from: 'a',
+    to: 'b',
+    amount: 1
+}
+
+const str = JSON.stringify(transaction);
+
+const hash = sha256(str).toString();
+
+console.log(hash);
